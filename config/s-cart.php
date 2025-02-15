6dcefe329045c5f9ff816484c4d501770e095578
agit <agita@eskalink.com>
2025-02-15 20:57:08 +0700

    initiate commit

---
+++ config/s-cart.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'name'        => 'PT. Elementa Media Literasi',
+    'version'     => '1.0',
+    'sub-version' => '1.0.0',
+    'type'        => 'basic',
+    'homepage'    => 'https://elementamedia.id/',
+    'title'       => 'PT. Elementa Media Literasi',
+];
