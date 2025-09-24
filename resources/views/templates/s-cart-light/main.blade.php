fc4fea5000f927c3d17267b6d2ad59d3a0482ce2
Fedian Tegar Pradana <apedh.pradana@gmail.com>
2025-09-24 23:22:21 +0700

    update

--- resources/views/templates/s-cart-light/main.blade.php
+++ resources/views/templates/s-cart-light/main.blade.php
@@ -14,13 +14,12 @@
     <meta name="keywords" content="{{ $keyword ?? sc_store('keyword') }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{!! $title ?? sc_store('title') !!}</title>
     <link rel="icon" href="{{ sc_file(sc_store('icon', null, 'images/icon.png')) }}" type="image/png" sizes="16x16">
 
     {{-- Open Graph Meta --}}
     <meta property="og:type" content="website" />
     <meta property="og:url" content="{{ url()->current() }}" />
-    <meta property="og:title" content="{!! urldecode($desc->name ?? $product->alias) !!}">
+    <meta property="og:title" content="{!! $title ?? sc_store('title') !!}">
     <meta property="og:description" content="{{ urldecode($description ?? sc_store('description')) }}" />
     <meta property="og:image" content="{{ !empty($og_image) ? sc_file($og_image) : sc_file(sc_store('og_image')) }}" />
 
